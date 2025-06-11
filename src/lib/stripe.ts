5870c1ae276058abe148bdc4a55a87fdf1fbfe1a
AjayTeotia <teotiaajay20@gmail.com>
2025-06-11 13:57:43 +0530

    small-changes

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,7 +4,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2025-05-28.basil",
+    apiVersion: "2025-02-24.acacia",
     typescript: true,
 });
 
